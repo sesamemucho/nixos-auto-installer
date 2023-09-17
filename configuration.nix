643e6c14500d0296f7af024bdd5084d7206d6b33
Bob Forgey <rforgey@grumpydogconsulting.com>
2023-09-17 16:45:38 -0700

    Customizing to local network.

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,11 @@
   ];
   nixpkgs.config.allowUnfree = true;
   networking.hostName = "nixos-bootstrap";
+  networking.wireless = {
+    enable = true;
+    networks."gill".psk = "c0tton6Top3';
+    userControlled.enable = true;
+  };
   services.avahi = {
     enable = true;
     ipv4 = true;
@@ -25,6 +30,6 @@
   services.openssh.permitRootLogin = "yes";
   users.mutableUsers = false;
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIManOIJLP/sPj7glqo6R1+6+oJJToiNxtbHOLPPU187y tfc@jongebook"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJeeTzj074ZKNK3iE9OMaD8C/77SgbGIGkYCFr0bMk bob"
   ];
 }
